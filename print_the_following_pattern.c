740b77244099acb75c7083dbbc279fee3fb27384
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2022-12-22 20:28:40 +0530

    #include<stdio.h> int main() { int n, j,a; scanf("%d",&a); for (n = a; n >= 1; n--) {      for (j = a; j >= 1; j-- )      {           printf("%d ", j);      }      printf(" "); } }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n, j,a;
+scanf("%d",&a);
+for (n = a; n >= 1; n--)
+{
+     for (j = a; j >= 1; j-- )
+     {
+          printf("%d ", j);
+     }
+     printf("
+");
+}
+}
