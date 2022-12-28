e0183510cf961a9759b4bbdc768a3ac3a2ee6e2e
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2022-12-28 21:45:56 +0530

    #include<stdio.h> int main() {     int n,a,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&a);         if(a<3)         {             printf("LIGHT ");         }         else if(3<=a && a<7)         {             printf("MODERATE ");         }         else         {             printf("HEAVY ");         }     } }

---
+++ Rain_gauge.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a);
+        if(a<3)
+        {
+            printf("LIGHT
+");
+        }
+        else if(3<=a && a<7)
+        {
+            printf("MODERATE
+");
+        }
+        else
+        {
+            printf("HEAVY
+");
+        }
+    }
+}
