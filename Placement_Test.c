613599928defd7e507471901b46cbb8340ab6e43
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-14 21:47:48 +0530

    #include<stdio.h> int main(){     int n,x,tot,w;     scanf("%d",&n);     for(int i=0;i<n;i++){     scanf("%d",&tot);     scanf("%d",&w);     int x=(tot/10)*w;     printf("%d ",x);} }

---
+++ Placement_Test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,x,tot,w;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+    scanf("%d",&tot);
+    scanf("%d",&w);
+    int x=(tot/10)*w;
+    printf("%d
+",x);}
+}
