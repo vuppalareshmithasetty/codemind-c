8ed31f37ce3d05443b945c4c23f3c5e7bc828475
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-09-17 12:00:29 +0530

    #include<stdio.h> int main(){     int a[100],i,n,s=0;     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             s=s+a[i];         }     }     printf("%d",s); }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,x=0;
+int main(){
+    int a[100],i,n,s=0;
     scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
         if(i%2==0)
         {
-            x=x+a[i];
+            s=s+a[i];
         }
     }
-    printf("%d",x);
+    printf("%d",s);
 }
